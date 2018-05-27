b188726fef6334e993b9d97700a7b1cb6db9ae3b
Fabien Martin <fabien.martin87@gmail.com>
2018-05-27 15:23:48 +0800

    add expensify styles

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = (env) => {
     const isProduction = env === 'production'
     const CSSExtract = new ExtractTextPlugin('styles.css')
     return {
-        entry: "./src/app.js",
+        entry: ['babel-polyfill', './src/app.js'],
         output: {
             path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js'
@@ -57,7 +57,7 @@ module.exports = (env) => {
         ],
         devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
-            contentBase: path.join(__dirname, "public"),
+            contentBase: path.join(__dirname, 'public'),
             historyApiFallback: true,
             publicPath: '/dist/'
         } 
